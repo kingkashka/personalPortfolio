6decec4a39532863e6f5fc354d7e98c9647b5352
nicholas keyes <bubbzthegreat@gmail.com>
2024-04-24 18:52:50 -0500

    pics added

--- client/src/componentData/profileData.js
+++ client/src/componentData/profileData.js
@@ -1,7 +1,7 @@
 const profileData = [
     {
-        image: "/src/images/_MG_8404.jpg",
-        backImage: "/src/images/_MG_8406.jpg"
+        image: "/images/_MG_8404.jpg",
+        backImage: "/images/_MG_8406.jpg"
     }
 ]
 export default profileData
